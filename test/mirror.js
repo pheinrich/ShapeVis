32b7c4ec25caebcac4481f4b2757052555116977
Peter Heinrich <peter.heinrich@gmail.com>
2012-03-27 22:12:32 -0700

    Playing around with default tab content and CSS

---
+++ test/mirror.js
@@ -0,0 +1,14 @@
+function ShapeVis( that )
+{
+}
+
+ShapeVis.tabTemplate = function( href, label )
+{
+    return( "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>" );
+}
+
+ShapeVis.tabContent = function()
+{
+    return();
+}
+
